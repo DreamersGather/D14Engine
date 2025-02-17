8c65ad79d967b643498700e3ce4e8fee26878af0
yiyaowen <wenyiyao_fort@163.com>
2025-02-18 00:31:10 +0800

    Add sortxml.cs

---
+++ format.ps1
@@ -0,0 +1,21 @@
+﻿function D14UIKit-Run-SortXML
+{
+    param($XMLFilePath)
+    Write-Output "Format $XMLFilePath"
+    & ".\\sortxml.exe" "--overwrite" $XMLFilePath | Out-Null
+}
+$executablePath = Get-Command "csc.exe" -ErrorAction SilentlyContinue
+if ($executablePath)
+{
+    # Compile sortxml
+    & $executablePath "sortxml.cs" | Out-Null
+
+    # Format D14Engine
+    # D14UIKit-Run-SortXML -XMLFilePath "D14Engine.vcxproj"
+    # D14UIKit-Run-SortXML -XMLFilePath "D14Engine.vcxproj.filters"
+    # D14UIKit-Run-SortXML -XMLFilePath "D14Engine.vcxproj.user"
+}
+else # Keep everything unchanged.
+{
+    Write-Output "Warning: csc.exe not found, no changes have been made."
+}
