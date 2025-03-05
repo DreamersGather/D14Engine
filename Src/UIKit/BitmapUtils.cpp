5f30dbe4e43e49366315e27b31008cf303b64d25
yiyaowen <wenyiyao_fort@163.com>
2025-03-05 23:38:44 +0800

    Update BitmapUtils

--- Src/UIKit/BitmapUtils.cpp
+++ Src/UIKit/BitmapUtils.cpp
@@ -21,58 +21,71 @@ namespace d14engine::uikit::bitmap_utils
 
         auto factory = graph_utils::bitmap::factory();
 
-        // Create file and stream.
+        /////////////////////////////
+        // Create file and stream. //
+        /////////////////////////////
 
-        ComPtr<IWICStream> stream;
+        ComPtr<IWICStream> stream = {};
         THROW_IF_FAILED(factory->CreateStream(&stream));
 
-        THROW_IF_FAILED(stream->InitializeFromFilename(
-            imagePath.c_str(),
-            GENERIC_WRITE));
+        THROW_IF_FAILED(stream->InitializeFromFilename
+        (
+        /* wzFileName      */ imagePath.c_str(),
+        /* dwDesiredAccess */ GENERIC_WRITE
+        ));
 
-        // Export image to stream.
+        /////////////////////////////
+        // Export image to stream. //
+        /////////////////////////////
 
-        ComPtr<IWICBitmapEncoder> bitmapEncoder;
-        THROW_IF_FAILED(factory->CreateEncoder(
-            format,
-            nullptr,
-            &bitmapEncoder));
+        ComPtr<IWICBitmapEncoder> bitmapEncoder = {};
 
-        THROW_IF_FAILED(bitmapEncoder->Initialize(
-            stream.Get(),
-            WICBitmapEncoderNoCache));
+        THROW_IF_FAILED(factory->CreateEncoder
+        (
+        /* guidContainerFormat */ format,
+        /* pguidVendor         */ nullptr,
+        /* ppIEncoder          */ &bitmapEncoder
+        ));
+        THROW_IF_FAILED(bitmapEncoder->Initialize
+        (
+        /* pIStream    */ stream.Get(),
+        /* cacheOption */ WICBitmapEncoderNoCache
+        ));
+        ComPtr<IWICBitmapFrameEncode> frameEncode = {};
 
-        ComPtr<IWICBitmapFrameEncode> frameEncode;
-        THROW_IF_FAILED(bitmapEncoder->CreateNewFrame(
-            &frameEncode,
-            nullptr));
-
+        THROW_IF_FAILED(bitmapEncoder->CreateNewFrame
+        (
+        /* ppIFrameEncode    */ &frameEncode,
+        /* ppIEncoderOptions */ nullptr
+        ));
         THROW_IF_FAILED(frameEncode->Initialize(nullptr));
 
-        auto device = Application::g_app->dx12Renderer()->d2d1Device();
+        ComPtr<IWICImageEncoder> imageEncoder = {};
 
-        ComPtr<IWICImageEncoder> imageEncoder;
-        THROW_IF_FAILED(factory->CreateImageEncoder(
-            device,
-            &imageEncoder));
-
-        WICImageParameters imageParams = {};
-        imageParams.PixelFormat = bitmap->GetPixelFormat();
-        bitmap->GetDpi(&imageParams.DpiX, &imageParams.DpiY);
-        imageParams.Top = 0.0f;
-        imageParams.Left = 0.0f;
+        THROW_IF_FAILED(factory->CreateImageEncoder
+        (
+        /* pD2DDevice        */ Application::g_app->dx12Renderer()->d2d1Device(),
+        /* ppWICImageEncoder */ &imageEncoder
+        ));
         auto pixSize = bitmap->GetPixelSize();
-        imageParams.PixelWidth = pixSize.width;
-        imageParams.PixelHeight = pixSize.height;
+        WICImageParameters imageParams =
+        {
+            .PixelFormat = bitmap->GetPixelFormat(),
+            .Top         = 0.0f,
+            .Left        = 0.0f,
+            .PixelWidth  = pixSize.width,
+            .PixelHeight = pixSize.height
+        };
+        bitmap->GetDpi(&imageParams.DpiX, &imageParams.DpiY);
 
-        THROW_IF_FAILED(imageEncoder->WriteFrame(
-            bitmap,
-            frameEncode.Get(),
-            &imageParams));
-
+        THROW_IF_FAILED(imageEncoder->WriteFrame
+        (
+        /* pImage           */ bitmap,
+        /* pFrameEncode     */ frameEncode.Get(),
+        /* pImageParameters */ &imageParams
+        ));
         THROW_IF_FAILED(frameEncode->Commit());
         THROW_IF_FAILED(bitmapEncoder->Commit());
-
         THROW_IF_FAILED(stream->Commit(STGC_DEFAULT));
     }
 
@@ -84,21 +97,21 @@ namespace d14engine::uikit::bitmap_utils
 
         D2D1_BITMAP_PROPERTIES1 props = D2D1::BitmapProperties1
         (
-            /* bitmapOptions */ options,
-            /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
-            /* dpiX          */ dpi,
-            /* dpiY          */ dpi
+        /* bitmapOptions */ options,
+        /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
+        /* dpiX          */ dpi,
+        /* dpiY          */ dpi
         );
-        ComPtr<ID2D1Bitmap1> bitmap;
-        // Hardcode the pixel format as R8G8B8A8 for simplicity, so the pitch
-        // (byte count of each scanline) is set to "4 * width" directly.
+        ComPtr<ID2D1Bitmap1> bitmap = {};
+        // Here we assume the pixel format is R8G8B8A8/B8G8R8A8 for simplicity,
+        // so the pitch (byte count of each scanline) is set to (4 * width).
         THROW_IF_FAILED(Application::g_app->dx12Renderer()->d2d1DeviceContext()->CreateBitmap
         (
-            /* size             */ { width, height },
-            /* sourceData       */ data,
-            /* pitch            */ 4 * width,
-            /* bitmapProperties */ props,
-            /* bitmap           */ &bitmap
+        /* size             */ { width, height },
+        /* sourceData       */ data,
+        /* pitch            */ 4 * width,
+        /* bitmapProperties */ props,
+        /* bitmap           */ &bitmap
         ));
         return bitmap;
     }
@@ -109,19 +122,21 @@ namespace d14engine::uikit::bitmap_utils
 
         auto source = graph_utils::bitmap::load(imagePath);
 
+        auto dpi = platform_utils::dpi();
+
         D2D1_BITMAP_PROPERTIES1 props = D2D1::BitmapProperties1
         (
-            /* bitmapOptions */ options,
-            /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
-            /* dpiX          */ 0.0f,
-            /* dpiY          */ 0.0f
+        /* bitmapOptions */ options,
+        /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
+        /* dpiX          */ dpi,
+        /* dpiY          */ dpi
         );
-        ComPtr<ID2D1Bitmap1> bitmap;
+        ComPtr<ID2D1Bitmap1> bitmap = {};
         THROW_IF_FAILED(Application::g_app->dx12Renderer()->d2d1DeviceContext()->CreateBitmapFromWicBitmap
         (
-            /* wicBitmapSource  */ source.Get(),
-            /* bitmapProperties */ props,
-            /* bitmap           */ &bitmap
+        /* wicBitmapSource  */ source.Get(),
+        /* bitmapProperties */ props,
+        /* bitmap           */ &bitmap
         ));
         return bitmap;
     }
@@ -130,38 +145,48 @@ namespace d14engine::uikit::bitmap_utils
     {
         THROW_IF_NULL(Application::g_app);
 
-        // Create stream from memory.
+        ////////////////////////////////
+        // Create stream from memory. //
+        ////////////////////////////////
 
         auto src = loadResource(resName, resType);
 
-        HGLOBAL	mem = GlobalAlloc(GMEM_MOVEABLE, src.size);
+        auto mem = GlobalAlloc(GMEM_MOVEABLE, src.size);
         THROW_IF_NULL(mem);
-
-        LPVOID dst = GlobalLock(mem);
+        auto dst = GlobalLock(mem);
         THROW_IF_NULL(dst);
-        memcpy(dst, src.data, src.size);
-        GlobalUnlock(mem);
 
-        ComPtr<IStream> stream;
+        // TODO: Can we avoid a copy here?
+        // (create a stream object directly with the memory pointer)
+        memcpy(dst, src.data, src.size);
+
+        GlobalUnlock(mem);
+        GlobalFree(mem);
+
+        ComPtr<IStream> stream = {};
         THROW_IF_FAILED(CreateStreamOnHGlobal(mem, FALSE, &stream));
 
-        // Create bitmap from stream.
+        ////////////////////////////////
+        // Create bitmap from stream. //
+        ////////////////////////////////
 
         auto source = graph_utils::bitmap::load(stream.Get());
 
+        auto dpi = platform_utils::dpi();
+
         D2D1_BITMAP_PROPERTIES1 props = D2D1::BitmapProperties1
         (
-            /* bitmapOptions */ options,
-            /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
-            /* dpiX          */ 0.0f,
-            /* dpiY          */ 0.0f
+        /* bitmapOptions */ options,
+        /* pixelFormat   */ D2D1::PixelFormat(Renderer::g_renderTargetFormat, D2D1_ALPHA_MODE_PREMULTIPLIED),
+        /* dpiX          */ dpi,
+        /* dpiY          */ dpi
         );
-        ComPtr<ID2D1Bitmap1> bitmap;
+        ComPtr<ID2D1Bitmap1> bitmap = {};
         THROW_IF_FAILED(Application::g_app->dx12Renderer()->d2d1DeviceContext()->CreateBitmapFromWicBitmap
         (
-            /* wicBitmapSource  */ source.Get(),
-            /* bitmapProperties */ props,
-            /* bitmap           */ &bitmap
+        /* wicBitmapSource  */ source.Get(),
+        /* bitmapProperties */ props,
+        /* bitmap           */ &bitmap
         ));
         return bitmap;
     }
